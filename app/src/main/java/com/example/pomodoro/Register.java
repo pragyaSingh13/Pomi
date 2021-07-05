595fee2b83cb4320f26b44780eb89fca8a450629
Pragya Singh <pragyaprogrammer135@gmail.com>
2021-07-05 15:16:53 +0530

    signup changes

--- app/src/main/java/com/example/pomodoro/Register.java
+++ app/src/main/java/com/example/pomodoro/Register.java
@@ -20,88 +20,86 @@ import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class Register extends AppCompatActivity {
- SignInButton signInButton ;
- static FirebaseAuth mAuth;
- static GoogleSignInOptions gso;
- static EditText emailV;
- static EditText passwordV;
- static GoogleSignInClient mGoogleSignInClient;
+    SignInButton signInButton;
+    static FirebaseAuth mAuth;
+    static GoogleSignInOptions gso;
+    static GoogleSignInClient mGoogleSignInClient;
+    private int RC_SIGN_IN = 007;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-        mAuth = FirebaseAuth.getInstance();
-        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.default_web_client_id))
-                .requestEmail()
-                .build();
-        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        createReq();
+        findViewById(R.id.signinbtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signIn();
+            }
+        });
+
 
     }
 
-
-    public void clicked(View view) {
-        emailV = findViewById(R.id.email1);
-        passwordV = findViewById(R.id.pass1);
-        mAuth = FirebaseAuth.getInstance();
-
-        String email  = emailV.getText().toString();
-        String password = passwordV.getText().toString();
-
-        mAuth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d("tag", "createUserWithEmail:success");
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            //do shit
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Log.w("tag", "createUserWithEmail:failure", task.getException());
-                            Toast.makeText(Register.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
-                            //do shit
-                        }
-                    }
-                });
-
-    }
-
-    public void googleClick(View view) {
-        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+    private void createReq(){
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestIdToken(getString(R.string.default_web_client_id))
                 .requestEmail()
                 .build();
+
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+    }
+
+    private void signIn() {
         Intent signInIntent = mGoogleSignInClient.getSignInIntent();
-        startActivityForResult(signInIntent, 007);
-
+        startActivityForResult(signInIntent, RC_SIGN_IN);
     }
+    @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        String id;
 
         // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
-        if (requestCode == 007 ){
+        if (requestCode == RC_SIGN_IN) {
             Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
             try {
                 // Google Sign In was successful, authenticate with Firebase
                 GoogleSignInAccount account = task.getResult(ApiException.class);
-                id = account.getIdToken();
-                Toast.makeText(this, "Success"+id, Toast.LENGTH_SHORT).show();
-                startActivity(new Intent(Register.this,home.class));
-
+                Toast.makeText(this, "Success!", Toast.LENGTH_SHORT).show();
+                firebaseAuthWithGoogle(account.getIdToken());
             } catch (ApiException e) {
                 // Google Sign In failed, update UI appropriately
-                Toast.makeText(this, "Google sign in failed", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "Oops! Something went wrong", Toast.LENGTH_SHORT).show();
             }
         }
-    }
+
 }
+
+    private void firebaseAuthWithGoogle(String idToken) {
+
+            AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+            mAuth.signInWithCredential(credential)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Log.d("tag", "signInWithCredential:success");
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                updateUI(user);
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Log.w("tag", "signInWithCredential:failure", task.getException());
+                                updateUI(null);
+                            }
+                        }
+                    });
+
+    }
+    }
